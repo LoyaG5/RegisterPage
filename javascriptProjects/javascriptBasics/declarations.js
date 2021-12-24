693d3132c9c80c75ebeb6f0401d2e198d5c4006d
“LoyaG5” <Loya.GM01@gmail.com>
2021-12-23 19:28:27 -0500

    second commit

---
+++ javascriptProjects/javascriptBasics/declarations.js
@@ -0,0 +1,12 @@
+//VARIABLES
+// variables are named containers for storing data
+
+// SYNTAX
+let nameOfVariable = "value"
+//(1)    (2)   (3)   (4)
+/*
+1. let is a javascript keyword to denote the creation of a variable
+2. Name of the varaible which we can use ti access the value
+3. = is used to assign a value. We call this the assignment operator
+4. The initial value
+*/
